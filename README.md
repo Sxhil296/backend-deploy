fd8a90a4cf4b8cfbfeee9072bb3430ed85bb6ecd
Sahil <leviack629@gmail.com>
2023-10-26 22:49:41 +0530

    json data

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # backend-deploy
+
+
+
+## push on github first
+## go to digital ocean or any other platform
+## create a new droplet or app(prefer app)
+## clone your repo there from github
+## select stable branch
+## select plan
+## go to env variables and add them
+
+
--- index.js
+++ index.js
@@ -5,6 +5,41 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const githubData = {
+    "login": "Sxhil296",
+    "id": 108786416,
+    "node_id": "U_kgDOBnvy8A",
+    "avatar_url": "https://avatars.githubusercontent.com/u/108786416?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Sxhil296",
+    "html_url": "https://github.com/Sxhil296",
+    "followers_url": "https://api.github.com/users/Sxhil296/followers",
+    "following_url": "https://api.github.com/users/Sxhil296/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Sxhil296/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Sxhil296/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Sxhil296/subscriptions",
+    "organizations_url": "https://api.github.com/users/Sxhil296/orgs",
+    "repos_url": "https://api.github.com/users/Sxhil296/repos",
+    "events_url": "https://api.github.com/users/Sxhil296/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Sxhil296/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Sahil",
+    "company": null,
+    "blog": "https://sahil-malik.netlify.app/",
+    "location": null,
+    "email": null,
+    "hireable": true,
+    "bio": "Aspiring Full Stack Web Developer",
+    "twitter_username": "tf3mir",
+    "public_repos": 158,
+    "public_gists": 0,
+    "followers": 7,
+    "following": 10,
+    "created_at": "2022-07-06T06:25:51Z",
+    "updated_at": "2023-10-22T11:02:19Z"
+  }
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -20,6 +55,10 @@ app.get("/login", (req, res) => {
 app.get('/chai', (req, res) => {
     res.send('<h2>Chai is the best!</h2>');
 } )
+
+app.get("/github", (req, res) => {
+    res.json(githubData)
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
